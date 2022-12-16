1758a483a952ee1858a36496575596ef73a7e3da
Jake Watembach <jakewatembach@gmail.com>
2022-12-15 20:48:51 -0700

    Initial Commit

---
+++ dateTrack/dateTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateTrackApp.swift
+//  dateTrack
+//
+//  Created by Jake Watembach on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct dateTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
